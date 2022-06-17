ee9d69381313835f462e25f06f944498d891a5df
Ahmed Khaled <91360901+Ahmed-Khaled11@users.noreply.github.com>
2022-06-17 03:12:24 -0700

    Update [id].js

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -131,7 +131,7 @@ export default function ProductDetails(props) {
 
 export async function getStaticProps(context) {
   const res = await fetch(
-    `http://localhost:3000/api/products/${context.params.id}`
+    `https://pizza-clone.vercel.app/products/${context.params.id}`
   );
   const data = await res.json();
   return {
